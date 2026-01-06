530946c4ae76f527870cfa7cc3cc2ad0d41c2743
Josue049 <josue09guti@gmail.com>
2026-01-05 21:25:35 -0500

    feature(GithubPages): DS3 Comunicaciones

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default {
-  base: "/Ds3Peru-Responsive/"
-}
-
+export default defineConfig({
+  plugins: [react()],
+  base: "/Ds3Peru-Responsive/" // <- tu repo
+});
